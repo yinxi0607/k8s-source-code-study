f9ea66291802cabb98ce2446da170b6b67428a52
niuhongbin <niuhongbin111@163.com>
2023-08-27 17:26:48 +0800

    add informer example

--- informer/main.go
+++ informer/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
+	"time"
+)
 
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", "./.kube/config")
+	if err != nil {
+		panic(err)
+	}
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	sharedInformers := informers.NewSharedInformerFactory(clientSet, time.Minute)
+	informer := sharedInformers.Core().V1().Pods().Informer()
+	_, _ = informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			mObj := obj.(v1.Object)
+			log.Printf("New Pod Added to Store: %s", mObj.GetName())
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			oObj := oldObj.(v1.Object)
+			nObj := newObj.(v1.Object)
+			log.Printf("Pod Updated in Store: Old: %s, New: %s", oObj.GetName(), nObj.GetName())
+		},
+		DeleteFunc: func(obj interface{}) {
+			mObj := obj.(v1.Object)
+			log.Printf("Pod Deleted from Store: %s", mObj.GetName())
+		},
+	})
+
+	informer.Run(stopCh)
 }
